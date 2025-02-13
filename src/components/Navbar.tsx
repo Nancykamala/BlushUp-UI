64dcd3353ca055ec9335b58867b5a7b74862ed38
NancyKamala <anancykamala@gmail.com>
2025-02-13 10:56:59 +0530

    added product imgaes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Image from "next/image";
+import logo from "../../public/images/blushup-logo.png"
 
 const Navbar = () => {
   return (
     <AppBar position="sticky" color="primary">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          BlushUp
-        </Typography>
+        {/* Logo and Text Container */}
+        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+        <Image src={logo} alt="BlushUp Logo" width={40} height={40} />
+          <Typography variant="h6">BlushUp</Typography>
+        </Box>
+
+        {/* Cart Icon */}
         <IconButton color="inherit">
           <ShoppingCartIcon />
         </IconButton>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,28 @@
 import { Card, CardMedia, CardContent, Typography, Button, Box } from "@mui/material";
-import LakmeLipcolor from "../public/images/LakmeLipcolor.png"
 
 interface ProductProps {
   product: {
     id: number;
     name: string;
     price: number;
-    image: string;
   };
 }
 
+// Function to dynamically require images
+const getProductImage = (id: number) => {
+  try {
+    return `/images/product-${id}.png`;
+  } catch (error) {
+    return `/images/default.png`;
+  }
+};
+
 const ProductCard: React.FC<ProductProps> = ({ product }) => {
+  const imageUrl = getProductImage(product.id);
+
   return (
     <Card sx={{ maxWidth: 300, m: 2, p: 2, boxShadow: 3 }}>
-      <CardMedia component="img" height="200" image={"LakmeLipcolor"} alt={product.name} />
+      <CardMedia component="img" height="200" image={imageUrl} alt={product.name} />
       <CardContent>
         <Typography variant="h6" noWrap>{product.name}</Typography>
         <Typography color="text.secondary">${product.price.toFixed(2)}</Typography>
